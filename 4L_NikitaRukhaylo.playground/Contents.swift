0cbbaea254faa3a4c2f8974fb112623cd5bb331a
Nikita Rukhaylo <kikitosina@gmail.com>
2020-10-29 19:24:09 +0300

    finished

--- 4L_NikitaRukhaylo.playground/Contents.swift
+++ 4L_NikitaRukhaylo.playground/Contents.swift
@@ -1,3 +1,77 @@
 import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+
+enum EngineStatus {
+    case isOn
+    case isOff
+}
+
+enum Action {
+    case setEngine(state: EngineStatus)
+    case setLoad(amount: Int)
+}
+
+class Car {
+    
+    var doors: Int
+    var hp: Int
+    var engine: EngineStatus = .isOff
+    var load = 0
+    var color: String
+    
+    init(hp: Int, doors: Int, color: String) {
+        self.hp = hp
+        self.doors = doors
+        self.color = color
+    }
+    
+    func action(action: Action) {
+        
+    }
+}
+
+class trunkCar: Car {
+    
+    init(hp: Int) {
+        super.init(hp: hp, doors: 4, color: "black")
+    }
+    
+    override func action(action: Action) {
+        switch action {
+        case .setEngine(state: let state):
+            self.engine = state
+        case .setLoad(amount: let amount):
+            self.load += amount
+        }
+    }
+    
+}
+
+class sportCar: Car {
+    
+    init(hp: Int) {
+        super.init(hp: hp, doors: 2, color: "red")
+    }
+    override func action(action: Action) {
+        switch action {
+        case .setEngine(state: let state):
+            self.engine = state
+        case .setLoad(amount: let amount):
+            self.load += amount
+        }
+    }
+    
+}
+
+let fiat = trunkCar(hp: 1500)
+print(fiat.engine, fiat.load, fiat.doors, fiat.hp, fiat.color)
+fiat.action(action: .setEngine(state: .isOn))
+fiat.action(action: .setLoad(amount: 10000))
+print(fiat.engine, fiat.load, fiat.doors, fiat.hp, fiat.color)
+
+let ferrari = sportCar(hp: 800)
+print(ferrari.engine, ferrari.load, ferrari.doors, ferrari.hp, ferrari.color)
+ferrari.action(action: .setEngine(state: .isOn))
+ferrari.action(action: .setLoad(amount: 160))
+print(ferrari.engine, ferrari.load, ferrari.doors, ferrari.hp, ferrari.color)
